d768399538e7cc2ca423791e85b0e4f158e3b15d
sdixon <stephen.dixon@ukaea.uk>
2022-02-08 20:37:19 +0000

    SDT-88 fixing vector allocation errors

--- include/materials/VectorTrappingMaterial.h
+++ include/materials/VectorTrappingMaterial.h
@@ -1,12 +1,5 @@
 // This file is part of the MOOSE framework
 // https://www.mooseframework.org
-//
-// All rights reserved, see COPYRIGHT for full restrictions
-// https://github.com/idaholab/moose/blob/master/COPYRIGHT
-//
-// Licensed under LGPL 2.1, please see LICENSE for details
-// https://www.gnu.org/licenses/lgpl-2.1.html
-
 #pragma once
 
 #include "ADMaterial.h"
@@ -36,22 +29,11 @@ private:
   Real _E_diff;
   Real _lambda;
   Real _n_sol;
-  // Real _rho;
   std::vector<Real> _ni;
-  Real _conductivity;
-  Real _cp;
-  Real _density;
-  // Real _T;
-  // Real _xp;
 
   const ADVariableValue & _T;
   ADMaterialProperty<Real> & _D;
   ADMaterialProperty<Real> & _Vm;
   ADMaterialProperty<std::vector<Real>> & _Vi;
   ADMaterialProperty<std::vector<Real>> & _n;
-
-  ADMaterialProperty<Real> & _Thermal_diffusivity;
-  ADMaterialProperty<Real> & _Conductivity;
-  ADMaterialProperty<Real> & _Density;
-  ADMaterialProperty<Real> & _Cp;
 };
